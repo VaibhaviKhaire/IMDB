0e8696c8275fb5e0e9233fd609753551b1cf8cb9
Vaibhavi <vaibhavikhire81@gmail.com>
2022-09-16 02:50:25 +0530

    Chnages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Provider } from 'react-redux'
+import store from './store'
+import Movies from './components/Movies';
+import styles from "./components/imdb.module.css"
+import AddForm from './components/AddForm';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className={styles.container}>
+        <div className={styles.sub_container}>
+          <h2 className='title'>My Movies</h2>
+        <Movies/></div>
+        <div className={styles.sub_container}>
+        <h2 className='title'>Movie Form</h2>
+        <AddForm/></div>
+        
+      </div>
+      
+    </Provider>
   );
 }
 
